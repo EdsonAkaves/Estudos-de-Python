20b4af4dd024f67d1d69a59d4a9a979a73f2020f
Edson Alves <edson.akaves@gmail.com>
2023-07-01 14:27:03 -0300

    Adicionando arquivo ex002

---
+++ ex002.py
@@ -0,0 +1,5 @@
+day = input('Em que dia do mês você nasceu? ')
+month = input('Em qual mês você nasceu? ')
+year = input('Em qual ano você nasceu? ')
+
+print('Você nasceu em', day, 'de' , month, 'de', year)
