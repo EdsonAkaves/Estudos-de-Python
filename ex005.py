536a58e256f40355f7d689545e3a164f648d2683
Edson Alves <edson.akaves@gmail.com>
2023-07-04 21:28:52 -0300

    Adicionando exercícios exx05 e ex006

---
+++ ex005.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia um número inteiro e mostre na tela  o seu sucessor e o seu antecessor.
+
+n = int(input('Digite um número: '))
+print(f'O sucessor de {n} é {n+1} e o seu antecessor é {n-1}')
+
+
+
+
+
---
+++ ex006.py
@@ -0,0 +1,5 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+n = int(input('Digite um número: '))
+print('\n O dobro de {} é {}.\n O triplo de {} é {}.\n e sua raiz quadrada é {:.2f}'.format(n, n*2, n, n*3, (n**(1/2))))
+
+
