cec5644f7aa982046a5ea25d2453032d6c73df26
Edson Alves <edson.akaves@gmail.com>
2023-07-05 09:54:57 -0300

    Adicionando exercicios 007 e 008

---
+++ ex008.py
@@ -0,0 +1,4 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+n = int(input('Digite um valor em metros: '))
+print('{}m em centímetros é {}, e em milímetros é {}'.format(n, n*100, n*1000))
+
