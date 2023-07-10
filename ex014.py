95dc3f26561c641779d3f102ad13fe15f1d88ac9
Edson Alves <edson.akaves@gmail.com>
2023-07-10 09:08:53 -0300

    Adicionando exercício 014

---
+++ ex014.py
@@ -0,0 +1,11 @@
+#Escreva um programa que converta uma temperatura digitada em ºC para ºF.
+
+
+
+c = float(input('Digite uma temperatura em C: '))
+
+
+print('{}ºC corresponde a {:.1f}F'.format(c, 1.8 * c + 32))
+
+
+
